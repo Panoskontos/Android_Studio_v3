821477c111c0c0ab053e56008ea18cf11932fe6f
Panoskontos <panos96el@gmail.com>
2023-05-12 00:55:07 +0300

    first commit

--- app/src/main/java/com/example/myapplication4/MainActivity.java
+++ app/src/main/java/com/example/myapplication4/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.myapplication4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edittxt;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edittxt = findViewById(R.id.editTextTextPersonName2);
+        btn = findViewById(R.id.button);
+        btn.setOnClickListener(v -> {
+            Intent intent = new Intent(this, MainActivity2.class);
+            startActivity(intent);
+        });
+
     }
 }
