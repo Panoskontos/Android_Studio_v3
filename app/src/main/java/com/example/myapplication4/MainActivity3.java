7a9e32b25089fdfa8ea5787ad0ae57d5520bcdd0
Panoskontos <panos96el@gmail.com>
2023-05-12 01:31:21 +0300

    first commit

--- app/src/main/java/com/example/myapplication4/MainActivity3.java
+++ app/src/main/java/com/example/myapplication4/MainActivity3.java
@@ -3,12 +3,26 @@ package com.example.myapplication4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity3 extends AppCompatActivity {
+
+    EditText username, email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+        username = findViewById(R.id.editTextTextPersonName3);
+        email = findViewById(R.id.editTextTextPersonName4);
+
+    }
+
+    public void insert(View view){
+
+    }
+    public void select(View view){
+
     }
 }
