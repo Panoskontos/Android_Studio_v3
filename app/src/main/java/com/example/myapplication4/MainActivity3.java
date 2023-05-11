093955725f5cce1a644e25a16b15b0b247545ba5
Panoskontos <panos96el@gmail.com>
2023-05-12 02:10:24 +0300

    first commit

--- app/src/main/java/com/example/myapplication4/MainActivity3.java
+++ app/src/main/java/com/example/myapplication4/MainActivity3.java
@@ -2,10 +2,12 @@ package com.example.myapplication4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity3 extends AppCompatActivity {
 
@@ -29,9 +31,20 @@ public class MainActivity3 extends AppCompatActivity {
     }
 
     public void insert(View view){
-
+        String usernameStr = username.getText().toString();
+        String emailStr = email.getText().toString();
+        sqLiteDatabase.execSQL("INSERT OR IGNORE INTO USER VALUES(?,?)",new String[] {usernameStr,emailStr});
+        Toast.makeText(this, "User Added", Toast.LENGTH_SHORT).show();
     }
     public void select(View view){
 
+    }
+
+    private void showMessage(String title, String msg){
+        new AlertDialog.Builder(this)
+                .setCancelable(true)
+                .setTitle(title)
+                .setMessage(msg)
+                .show();
     }
 }
